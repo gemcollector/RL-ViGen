89084c7f7f89753ae8d07674c92bf46a06824268
gemcollector <491525377@qq.com>
2023-08-06 11:54:48 +0800

    clear readme

--- install_adroit.sh
+++ install_adroit.sh
@@ -5,10 +5,7 @@ pip install dm-control==0.0.425341097
 pip install absl-py==0.13.0 pyparsing==2.4.7 jupyterlab==3.0.14 scikit-image \
     termcolor==1.1.0 imageio-ffmpeg==0.4.4 hydra-core==1.1.0 hydra-submitit-launcher==1.1.5 ipdb==0.13.9 \
     yapf==0.31.0 opencv-python==4.5.3.56 psutil tb-nightly
-#git clone https://github.com/watchernyu/rrl-dependencies.git \
-#    && cd rrl-dependencies \
-#    && pip install -e mj_envs/. \
-#    && pip install -e mjrl/.
+
 pip install joblib==1.2.0
 cd .. 
 cd DHM && pip install -e .
